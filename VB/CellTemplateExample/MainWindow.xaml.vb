e3e978a86152dc27962ca699f416c4dd5e649cdf
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/CellTemplateExample/MainWindow.xaml.vb
@@ -0,0 +1,15 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+
+Namespace CellTemplateExample
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			spreadsheetControl1.LoadDocument("vlookup.xlsx")
+'			#Region "#celltemplate_code"
+			spreadsheetControl1.CellTemplate = TryCast(spreadsheetControl1.TryFindResource("FormulaTemplate"), DataTemplate)
+'			#End Region ' #celltemplate_code
+		End Sub
+	End Class
+End Namespace
