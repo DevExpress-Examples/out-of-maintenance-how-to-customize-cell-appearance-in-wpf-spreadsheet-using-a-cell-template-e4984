753c10ff7e660ede11e45512f88703882be47d44
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CellTemplateExample/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace CellTemplateExample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            spreadsheetControl1.LoadDocument("vlookup.xlsx");
+            #region #celltemplate_code
+            spreadsheetControl1.CellTemplate = 
+                spreadsheetControl1.TryFindResource("FormulaTemplate") as DataTemplate;
+            #endregion #celltemplate_code
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to customize cell appearance in WPF Spreadsheet using a cell template
+
+
+<p>In this example the <strong>SpreadsheetControl.CellTemplate</strong> property is used to specify a data template that displays a cell formula near the cell.</p><p><img src="https://raw.githubusercontent.com/DevExpress-Examples/how-to-customize-cell-appearance-in-wpf-spreadsheet-using-a-cell-template-e4984/13.2.5+/media/6cd8416c-bd71-4c5d-a567-2a192521bbd5.png"></p>
+
+<br/>
+
+
